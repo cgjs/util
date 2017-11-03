5e396adbf1e2020fe19a655b0f46c89b981e7e5a
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2017-11-03 11:24:33 -0300

    first release

--- test/index.js
+++ test/index.js
@@ -1,5 +1,24 @@
-const MODULE = require('../index');
+const util = require('../index');
 
-if (!Object.keys(MODULE).length) {
-  throw new Error('MODULE was not exported');
+console.log(
+  util.inspect({
+    n: 123,
+    f: function () {},
+    a: [1, '2', 3, null],
+    o: {a: [1, {}, 3]}
+  },
+  {colors: true}
+));
+
+console.log(
+  util.isDeepStrictEqual(
+    {a: 123, b: [1, 2, 3]},
+    {a: 123, b: [1, 2, 3]}
+  )
+);
+
+function readAsync(name, cb) {
+  setTimeout(cb, 100, null, `OK ${name}`);
 }
+
+util.promisify(readAsync)('test').then(console.log);
